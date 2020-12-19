5dc92536d8057a0e5567015c1e12700b000de0df
Romart <rmf1312@hotmail.com>
2020-12-19 20:44:48 +0000

    Initial commit

---
+++ dropin.js
@@ -0,0 +1,23 @@
+// 0. Get clientKey
+getClientKey().then(clientKey => {
+    getPaymentMethods().then(paymentMethodsResponse => {
+        // 1. Create an instance of AdyenCheckout
+        const checkout = new AdyenCheckout({
+            environment: 'test',
+            clientKey: 'test_H5T4RJ2U5ZCALOYK46JK7T6IK4RW655F', 
+            paymentMethodsResponse,
+        });
+
+        // 2. Create and mount the Component
+        const dropin = checkout
+            .create('dropin', {
+                // Event
+                onSubmit: (state, component) => {
+                    // state.data;
+                    // state.isValid;
+                    makePayment(state.data);
+                }
+            })
+            .mount('#dropin-container');
+    });
+});
